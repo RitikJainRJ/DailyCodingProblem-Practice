01148fe0cdb36c67bf5476e0e3d0b39233a6c0c0
Ritik Jain <ritikjain1002@gmail.com>
2020-01-14 11:41:59 +0530

    Update Maximum of all subarrays of size k.cpp

--- Maximum of all subarrays of size k.cpp
+++ Maximum of all subarrays of size k.cpp
@@ -1,1 +1,41 @@
+/*
+Given an array A and an integer K. Find the maximum for each and every contiguous subarray of size K.
+*/
+#include<bits/stdc++.h>
+using namespace std;
 
+int solve(int, int);
+
+int main(){
+    int t, n, k;
+
+    cin >> t;
+    while(t--){
+        cin >> n >> k;
+        solve(n, k);
+        cout << endl;
+    }
+    return 0;
+}
+
+int solve(int n, int k){
+    int arr[n];
+    deque<int> dq(k);
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < k; i++){
+        while((!dq.empty()) && arr[dq.back()] <= arr[i])
+            dq.pop_back();
+        dq.push_back(i);
+    }
+    for(int i = k; i < n; i++){
+        cout << arr[dq.front()] << " ";
+        while((!dq.empty()) && dq.front() <= i - k)
+            dq.pop_front();
+        while((!dq.empty()) && arr[dq.back()] <= arr[i])
+            dq.pop_back();
+        dq.push_back(i);
+    }
+    cout << arr[dq.front()];
+}
